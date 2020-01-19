835fb7f521caf67ef20450db7ca2ddb0c06ba0ba
Shujaat Azim <shujaat.azim@gmail.com>
2020-01-18 23:40:42 -0500

    created a button for questions and categories, and set them up with a single function that GETs them both depending on the button pressed. Successfully logs the questions and categories from my api backend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 function App() {
+
+  const get = e => {
+    let name = e.target.name
+    fetch(`http://localhost:3000/${name}`)
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+  }
+
   return (
     <div>
       <h1>WoW Trivia!</h1>
+      <button onClick={get} name={"questions"}>Questions</button>
+      <button onClick={get} name={"categories"}>Categories</button>
     </div>
   );
 }
